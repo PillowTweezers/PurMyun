d2a7a97141b76b6217015136f67e36c6771abb01
Roi Klevansky <roiklevansky@gmail.com>
2022-01-19 19:59:30 +0200

    Fixed UI spacing with hebrew.

--- gui/ui/ui_mainwindow.py
+++ gui/ui/ui_mainwindow.py
@@ -8,15 +8,20 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
-                            QSize, Qt)
-from PySide6.QtGui import (QAction, QFont, QIcon)
-from PySide6.QtWidgets import (QAbstractScrollArea, QDockWidget, QHBoxLayout,
-                               QLineEdit, QMenu,
-                               QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
-                               QStatusBar, QTableWidget, QVBoxLayout,
-                               QWidget)
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QDockWidget, QHBoxLayout,
+    QHeaderView, QLineEdit, QMainWindow, QMenu,
+    QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
+    QStatusBar, QTableWidget, QTableWidgetItem, QVBoxLayout,
+    QWidget)
+import mainwindow_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
